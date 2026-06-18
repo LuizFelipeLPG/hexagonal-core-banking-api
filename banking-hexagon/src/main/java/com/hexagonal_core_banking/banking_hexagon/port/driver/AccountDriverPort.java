aebd05767b092df47abe896f80640d2db8c5307b
Luiz Felipe <luizfelipelpg2004@gmail.com>
2026-06-18 12:01:52 -0300

    feat(port): add Account driver and driven ports
    - Create AccountDriverPort interface to expose account operations
    - Create AccountDrivenPort interface to abstract account operations
    - Define methods for save, update, transfer, credit, debit and balance retrieval
    - Establish contracts between application and infrastructure layers

---
+++ banking-hexagon/src/main/java/com/hexagonal_core_banking/banking_hexagon/port/driver/AccountDriverPort.java
@@ -0,0 +1,14 @@
+package com.hexagonal_core_banking.banking_hexagon.port.driver;
+
+import com.hexagonal_core_banking.banking_hexagon.domain.dto.AccountDTO;
+import java.util.UUID;
+
+public interface AccountDriverPort {
+    AccountDTO save(AccountDTO accountDTO);
+    AccountDTO update(AccountDTO accountDTO);
+    AccountDTO findAccountById(UUID id);
+    void transfer(UUID from, UUID to, Double amount);
+    void credit(UUID id, Double amount);
+    void debit(UUID id, Double amount);
+    Double getBalance(UUID id);
+}
